3de0c41dc928066b7683d5cb1f57e3b490db8468
Avinash Jorige <jo.avi.1990@gmail.com>
2020-05-30 20:02:40 +0530

    Add project files.

---
+++ MVCAuthorization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAuthorization.Startup))]
+namespace MVCAuthorization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
