6c2c7c398ec0fd5b8c25d74c8e6b3f98f0a55355
Avinash Jorige <jo.avi.1990@gmail.com>
2020-05-30 22:57:13 +0530

    Initial Changes

--- MVCAuthorization/Controllers/AccountController.cs
+++ MVCAuthorization/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MVCAuthorization.Models;
+using MVCAuthorization.Repository;
+using System.Web.Security;
 
 namespace MVCAuthorization.Controllers
 {
@@ -66,30 +68,48 @@ namespace MVCAuthorization.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        //public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
+
+        //    // This doesn't count login failures towards account lockout
+        //    // To enable password failures to trigger account lockout, change to shouldLockout: true
+        //    var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+        //    switch (result)
+        //    {
+        //        case SignInStatus.Success:
+        //            return RedirectToLocal(returnUrl);
+        //        case SignInStatus.LockedOut:
+        //            return View("Lockout");
+        //        case SignInStatus.RequiresVerification:
+        //            return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+        //        case SignInStatus.Failure:
+        //        default:
+        //            ModelState.AddModelError("", "Invalid login attempt.");
+        //            return View(model);
+        //    }
+        //}
+
+        public ActionResult Login(LoginViewModel model, string returnUrl)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
 
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-            switch (result)
+            RepoDb repo = new RepoDb();
+
+            USER user = repo.GetUserDetails(model.Email, model.Password);
+
+            if(user != null)
             {
-                case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
-                case SignInStatus.LockedOut:
-                    return View("Lockout");
-                case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-                case SignInStatus.Failure:
-                default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
-                    return View(model);
+                FormsAuthentication.SetAuthCookie(user.UserName, model.RememberMe);''
             }
         }
+
 
         //
         // GET: /Account/VerifyCode
---
+++ MVCAuthorization/Repository/RepoDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAuthorization.Repository
+{
+    public class RepoDb
+    {
+        public USER GetUserDetails(string UserName,string Password)
+        {
+            USER user = new USER();
+            using (UserDBContext _context = new UserDBContext()) {
+                user  = _context.USERS.Where(u => u.UserName.ToLower() == UserName.ToLower() &&
+                                  u.Password == Password).FirstOrDefault();
+            }
+            return user;
+        }
+    }
+}
